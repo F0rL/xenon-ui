d845683b9e840ad238dd2090763fd1e39d0dd4f7
Forl <kumaloveslife@gmail.com>
2019-05-13 21:16:36 +0800

    初始化icon组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <h1>项目初始化</h1>
+    <y-button>按钮</y-button>
   </div>
 </template>
 
 <script>
+import Button from './components/basic/button/button'
+
+
 
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'y-button': Button,
+  }
 }
 </script>
 
 <style lang="scss">
+  #app {
+    max-width: 960px;
+    margin: 50px auto;
+  }
 </style>
---
+++ src/components/basic/button/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="x-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "yButton"
+  }
+</script>
+
+<style scoped lang="scss">
+  .x-button {
+    font-size: 14px;
+    height: 32px;
+    padding: 0 1em;
+    border-radius: 4px;
+    border: 1px solid #999;
+    background: #fff;
+    &:hover {
+      border-color: #666;
+    }
+    &:active {
+      background-color: #eee;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
