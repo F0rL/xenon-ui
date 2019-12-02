fc13db3e8c4127a59dc5cfe2d6d198a8c41752f4
F0rl <F0rL@users.noreply.github.com>
2019-12-02 21:34:46 +0800

    初步完成分割、空隙

--- packages/grid/col.vue
+++ packages/grid/col.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    col
+  <div class="xe-col" :class="colClass">
+    <slot></slot>
   </div>
 </template>
 
@@ -12,9 +12,39 @@ export default {
 
     }
   },
+  props: {
+    span: {
+      type: [String, Number],
+      required: true
+    },
+    offset: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  computed: {
+    colClass() {
+      return [`col-${this.span}`, `offset-${this.offset}`]
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+  .xe-col {
+    height: 100%;
+    border: 1px solid red;
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
 </style>
--- packages/grid/row.vue
+++ packages/grid/row.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    row
+  <div class="xe-row" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -9,13 +8,38 @@
 export default {
   name: 'xeRow',
   data() {
-    return {
-
+    return {}
+  },
+  props: {
+    align: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return ['left', 'right', 'center'].indexOf(value) >= 0
+      }
     }
   },
+  computed: {
+    rowClass() {
+      let align = this.align
+      return [align && `align-${align}`]
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.xe-row {
+  display: flex;
+  flex-wrap: wrap;
+  &.align-center {
+    justify-content: center;
+  }
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
+}
 </style>
