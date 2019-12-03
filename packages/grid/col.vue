328b767a4a6274d7d04dec6e7d49cfc0b9ed6599
F0rl <F0rL@users.noreply.github.com>
2019-12-03 17:23:41 +0800

    完成间隔功能

--- packages/grid/col.vue
+++ packages/grid/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="xe-col" :class="colClass">
+  <div class="xe-col" :class="colClass" :style="colStyle">
     <slot></slot>
   </div>
 </template>
@@ -9,7 +9,7 @@ export default {
   name: 'xeCol',
   data() {
     return {
-
+      gutter: 0
     }
   },
   props: {
@@ -18,33 +18,38 @@ export default {
       required: true
     },
     offset: {
-      type: [String, Number],
-      required: true
+      type: [String, Number]
     }
   },
   computed: {
     colClass() {
       return [`col-${this.span}`, `offset-${this.offset}`]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  .xe-col {
-    height: 100%;
-    border: 1px solid red;
-    $class-prefix: col-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n / 24) * 100%;
-      }
-    }
-    $class-prefix: offset-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        margin-left: ($n / 24) * 100%;
-      }
+.xe-col {
+  height: 100%;
+  border: 1px solid red;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
     }
   }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
+    }
+  }
+}
 </style>
--- packages/grid/row.vue
+++ packages/grid/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="xe-row" :class="rowClass">
+  <div class="xe-row" :class="rowClass" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -17,13 +17,25 @@ export default {
       validator(value) {
         return ['left', 'right', 'center'].indexOf(value) >= 0
       }
+    },
+    gutter: {
+      type: [Number, String]
     }
   },
   computed: {
     rowClass() {
       let align = this.align
       return [align && `align-${align}`]
+    },
+    rowStyle() {
+      let { gutter } = this
+      return { marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }
     }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
   }
 }
 </script>
